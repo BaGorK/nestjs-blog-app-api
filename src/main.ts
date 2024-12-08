b9466124f12ce300a07e557f10615748227b781c
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-12-08 17:05:23 +0300

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,18 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  /**
+   * swagger documentation
+   */
+  const config = new DocumentBuilder().setVersion('1.0').build();
+  /**
+   * Instantiate
+   */
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
