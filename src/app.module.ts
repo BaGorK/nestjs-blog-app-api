9c166371e0c0e67700865a8f6ef5989404d051e1
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-12-22 02:56:47 +0300

    feat: add Tags module with entity, controller, and integration into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         // synchronize: process.env.NODE_ENV === 'development',
       }),
     }),
+    TagsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
