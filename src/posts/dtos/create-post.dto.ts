4650b2b35a2bfe3db617e977d955cebcec82f1f8
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-12-22 01:42:41 +0300

    feat: add CreatePostDto with validation and implement Post entity for TypeORM

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -8,6 +8,7 @@ import {
   IsString,
   IsUrl,
   Matches,
+  MaxLength,
   MinLength,
   ValidateNested,
 } from 'class-validator';
@@ -24,6 +25,7 @@ export class CreatePostDto {
   })
   @IsString()
   @MinLength(4)
+  @MaxLength(512)
   @IsNotEmpty()
   title: string;
 
@@ -37,6 +39,8 @@ export class CreatePostDto {
     message:
       'A slug should be all small letters and uses only "-" as a separator without space. For example: "this-is-a-slug"',
   })
+  @MinLength(4)
+  @MaxLength(256)
   slug: string;
 
   @ApiProperty({
@@ -82,6 +86,7 @@ export class CreatePostDto {
   })
   @IsOptional()
   @IsUrl()
+  @MaxLength(1024)
   featuredImageUrl?: string;
 
   @ApiPropertyOptional({
