7eac19799d2d9306734453d8e9b861cd33142b46
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-12-26 16:32:57 +0300

    feat: enhance DTOs with Swagger API properties for better documentation

--- src/meta-options/dtos/create-post-meta-options.dto.ts
+++ src/meta-options/dtos/create-post-meta-options.dto.ts
@@ -1,6 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsJSON, IsNotEmpty } from 'class-validator';
 
 export class CreatePostMetaOptionsDto {
+  @ApiProperty({
+    type: 'string',
+    required: true,
+    description: 'The meta key',
+    example: '{"key": "value"}',
+  })
   @IsNotEmpty()
   @IsJSON()
   metaValue: string;
