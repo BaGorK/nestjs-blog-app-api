39ae266d82e23d317880779aa25159ea339fe459
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-12-26 20:21:00 +0300

    feat: build and complete Paginate response object

--- src/posts/providers/post.service.ts
+++ src/posts/providers/post.service.ts
@@ -12,6 +12,7 @@ import { TagsService } from 'src/tags/providers/tags.service';
 import { PatchPostDto } from '../dtos/patch-post.dto';
 import { GetPostsDto } from '../dtos/get-posts.dto';
 import { PaginationService } from 'src/common/pagination/providers/pagination.service';
+import { Paginated } from 'src/common/pagination/interfaces/pagination.interface';
 
 @Injectable()
 export class PostService {
@@ -62,23 +63,14 @@ export class PostService {
   /**
    * Get all posts
    */
-  public async findAll(postQuery: GetPostsDto) {
-    // return await this.postsRepository.find({
-    //   relations: {
-    //     metaOptions: true,
-    //     author: true,
-    //     tags: true,
-    //   },
-    //   take: postQuery.limit,
-    //   skip: (postQuery.page - 1) * postQuery.limit,
-    // });
-
+  public async findAll(postQuery: GetPostsDto): Promise<Paginated<Post>> {
     const posts = await this.paginationService.paginationQuery<Post>(
       {
         limit: postQuery.limit,
         page: postQuery.page,
       },
       this.postsRepository,
+      ['metaOptions', 'author', 'tags'], // Specify relations here
     );
 
     return posts;
