fe8b774b4e8e1a6bcc6dd110bbab02d96611ce30
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-12-27 21:01:11 +0300

    feat: add JWT configuration variables for secret, audience, issuer, and access token TTL

--- src/config/environment.validation.ts
+++ src/config/environment.validation.ts
@@ -14,4 +14,9 @@ export default Joi.object({
 
   // MODULE LEVEL CONFIG
   PROFILE_API_KEY: Joi.string().required(),
+
+  JWT_SECRET: Joi.string().required(),
+  JWT_TOKEN_AUDIENCE: Joi.string().required(),
+  JWT_TOKEN_ISSUER: Joi.string().required(),
+  JWT_ACCESS_TOKEN_TTL: Joi.number().default(3600),
 });
