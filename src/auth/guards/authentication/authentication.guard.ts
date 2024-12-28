418b4b19e49f946c15886b58a0a4bf3411a31fc6
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-12-28 15:30:34 +0300

    feat: enhance authentication flow with dynamic guard handling and JWT integration in users module

--- src/auth/guards/authentication/authentication.guard.ts
+++ src/auth/guards/authentication/authentication.guard.ts
@@ -1,8 +1,13 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
 import { AccessTokenGuard } from '../access-token/access-token.guard';
 import { AuthType } from 'src/auth/enums/auth-type.enum';
+import { AUTH_TYPE_KEY } from 'src/auth/constants/auth.constants';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
@@ -28,9 +33,32 @@ export class AuthenticationGuard implements CanActivate {
     private readonly accessTokenGuard: AccessTokenGuard,
   ) {}
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // authTypes from reflector
+    const authTypes = this.reflector.getAllAndOverride(AUTH_TYPE_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]) ?? [AuthenticationGuard.defaultAuthType];
+
+    // array of guards
+    const guards = authTypes
+      .map((authType) => this.authTypeGuardMap[authType])
+      .flat();
+
+    // Default error
+    const error = new UnauthorizedException();
+
+    // loop guards canActivates
+    for (const guard of guards) {
+      const canActivate = await Promise.resolve(
+        guard.canActivate(context),
+      ).catch((err) => ({ error: err }));
+
+      if (canActivate) {
+        return true;
+      }
+    }
+
+    throw error;
   }
 }
