f789c60c735eff48e7998ccd4e7ed4887f799fe0
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-01-05 13:30:41 +0300

    feat: implement generateTokens method for  generating both tokens at once

--- src/auth/providers/generate-token.provider.ts
+++ src/auth/providers/generate-token.provider.ts
@@ -2,6 +2,8 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import jwtConfig from '../config/jwt.config';
+import { User } from 'src/users/user.entity';
+import { ActiveUserData } from '../interfaces/active-user-data.interface';
 
 @Injectable()
 export class GenerateTokenProvider {
@@ -18,6 +20,9 @@ export class GenerateTokenProvider {
     private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
   ) {}
 
+  /**
+   * Sign token
+   */
   public async signToken<T>(userId: number, expiresIn: number, payload?: T) {
     const accessToken = await this.jwtService.signAsync(
       {
@@ -33,5 +38,30 @@ export class GenerateTokenProvider {
     );
 
     return accessToken;
+  }
+
+  public async generateTokens(user: User) {
+    const [accessToken, refreshToken] = await Promise.all([
+      // generate access token
+      this.signToken<Partial<ActiveUserData>>(
+        user.id,
+        this.jwtConfiguration.accessTokenTtl,
+        {
+          email: user.email,
+        },
+      ),
+
+      // generate refresh token
+      this.signToken<{ email: string }>(
+        user.id,
+        this.jwtConfiguration.refreshTokenTtl,
+        { email: user.email },
+      ),
+    ]);
+    // return both tokens
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }
