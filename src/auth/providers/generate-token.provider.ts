8c3c60cab5542f29d2853f9e8d67c5cdc90eb3ef
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-01-05 13:18:36 +0300

    feat: add GenerateTokenProvider for token generation in authentication flow

---
+++ src/auth/providers/generate-token.provider.ts
@@ -0,0 +1,37 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import jwtConfig from '../config/jwt.config';
+
+@Injectable()
+export class GenerateTokenProvider {
+  constructor(
+    /**
+     * Injecting JwtService
+     */
+    private readonly jwtService: JwtService,
+
+    /**
+     * Injecting jwt Config Service
+     */
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  public async signToken<T>(userId: number, expiresIn: number, payload?: T) {
+    const accessToken = await this.jwtService.signAsync(
+      {
+        sub: userId,
+        ...payload,
+      },
+      {
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+        expiresIn,
+      },
+    );
+
+    return accessToken;
+  }
+}
